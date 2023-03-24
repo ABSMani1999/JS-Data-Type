8e2c9a6b14df823b5631c7b8a440b76884b897af
Balasubramanian1999 <absmani1999@gmail.com>
2023-03-24 20:42:18 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,42 @@
+//data type
+//string
+var x='12';
+var y=8;
+
+document.write(x+y);
+document.write("<br>");
+
+//Number
+ let num1=3;
+ let num2=7;
+ let num3=5;
+ 
+document.write(num1+num2-num3);
+document.write("<br>");
+ 
+ //boolean
+ 
+ document.write(18>24);
+ document.write("<br>");
+ 
+ //undefined
+ 
+ let name;
+ 
+ document.write(name);
+ document.write("<br>");
+ 
+
+ 
+ 
+ 
+
+ 
+
+
+
+
+
+
+
+
